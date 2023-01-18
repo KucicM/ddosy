8ca5e1abe4d8a8b1804a004e5617cc7fcdd1459a
kucicm <10778553+KucicM@users.noreply.github.com>
2023-01-18 22:59:28 +0100

    tests

--- pkg/targeter_test.go
+++ pkg/targeter_test.go
@@ -9,12 +9,12 @@ import (
 )
 
 func TestSinglePattern(t *testing.T) {
-	req := ddosy.TrafficPatternWeb{
+	p := ddosy.TrafficPatternWeb{
 		Weight: 10,
 		Payload: "haha",
 	}
 	endpoint := "test-endpoint"
-	pattern := ddosy.NewTrafficPattern(endpoint, []ddosy.TrafficPatternWeb{req})
+	pattern := ddosy.NewTrafficPattern(endpoint, []ddosy.TrafficPatternWeb{p})
 
 	targeter := ddosy.NewWeightedTargeter(pattern)
 
@@ -23,7 +23,7 @@ func TestSinglePattern(t *testing.T) {
 		t.Errorf("got error from targeter %s", err)
 	}
 
-	expectedBody := []byte(req.Payload)
+	expectedBody := []byte(p.Payload)
 	if !reflect.DeepEqual(expectedBody, target.Body) {
 		t.Errorf("expected %v got %v", expectedBody, target.Body)
 	}
@@ -31,5 +31,37 @@ func TestSinglePattern(t *testing.T) {
 	if endpoint != target.URL {
 		t.Errorf("expected %v got %v", endpoint, target.URL)
 	}
+}
 
+func TestMultPattern(t *testing.T) {
+	p1 := ddosy.TrafficPatternWeb{
+		Weight: 10,
+		Payload: "1",
+	}
+	p2 := ddosy.TrafficPatternWeb{
+		Weight: 10,
+		Payload: "2",
+	}
+	endpoint := "test-endpoint"
+	pattern := ddosy.NewTrafficPattern(endpoint, []ddosy.TrafficPatternWeb{p1, p2})
+
+	targeter := ddosy.NewWeightedTargeter(pattern)
+
+	numOfIters := 1000
+	tracker := make(map[string]int)
+	for i := 0; i < numOfIters; i++ {
+		target := &vegeta.Target{}
+		targeter(target)
+		tracker[string(target.Body)] += 1
+	}
+
+	if len(tracker) != 2 {
+		t.Errorf("something dose not work with targeter, tracker size %+v", tracker)
+	}
+
+	for _, v := range tracker {
+		if v < 100 {
+			t.Errorf("Expected 50%% distribution got %d%%", v*100 / numOfIters)
+		}
+	}
 }
