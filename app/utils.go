6b147cbdbb004b0ceaa5b0833aaa20a7a2867686
kucicm <10778553+KucicM@users.noreply.github.com>
2023-01-22 15:57:37 +0100

    clean up

--- app/utils.go
+++ app/utils.go
@@ -1,6 +1,11 @@
 package ddosy
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
 
 func CreateDistribution(ws []float64) []float64 {
 	sum := 0.0
@@ -28,4 +33,17 @@ func RandomPick(ws []float64) int {
 		}
 	}
 	return len(ws) - 1
+}
+
+func NewWeightedTargeter(pattern TrafficPattern) vegeta.Targeter {
+	dist := pattern.dist
+	rand.Seed(time.Now().UnixNano())
+	return func(t *vegeta.Target) error {
+		i := RandomPick(dist.weigths)
+		t.Body = dist.payloads[i]
+		t.URL = pattern.endpoint
+		t.Method = pattern.method
+		t.Header = pattern.header
+		return nil
+	}
 }
--- test/utils_test.go
+++ test/utils_test.go
@@ -1,9 +1,11 @@
 package ddosy_test
 
 import (
+	"reflect"
 	"testing"
 
 	ddosy "github.com/kucicm/ddosy/app"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
 )
 
 func TestNormDistribution(t *testing.T) {
@@ -14,6 +16,64 @@ func TestNormDistribution(t *testing.T) {
 	for i, actual := range normDist {
 		if actual != expected[i] {
 			t.Errorf("expected %+v got %+v", expected, actual)
+		}
+	}
+}
+
+func TestSinglePattern(t *testing.T) {
+	p := ddosy.TrafficPatternWeb{
+		Weight:  10,
+		Payload: "haha",
+	}
+	endpoint := "test-endpoint"
+	pattern := ddosy.NewTrafficPattern(endpoint, []ddosy.TrafficPatternWeb{p})
+
+	targeter := ddosy.NewWeightedTargeter(pattern)
+
+	target := &vegeta.Target{}
+	if err := targeter(target); err != nil {
+		t.Errorf("got error from targeter %s", err)
+	}
+
+	expectedBody := []byte(p.Payload)
+	if !reflect.DeepEqual(expectedBody, target.Body) {
+		t.Errorf("expected %v got %v", expectedBody, target.Body)
+	}
+
+	if endpoint != target.URL {
+		t.Errorf("expected %v got %v", endpoint, target.URL)
+	}
+}
+
+func TestMultPattern(t *testing.T) {
+	p1 := ddosy.TrafficPatternWeb{
+		Weight:  10,
+		Payload: "1",
+	}
+	p2 := ddosy.TrafficPatternWeb{
+		Weight:  10,
+		Payload: "2",
+	}
+	endpoint := "test-endpoint"
+	pattern := ddosy.NewTrafficPattern(endpoint, []ddosy.TrafficPatternWeb{p1, p2})
+
+	targeter := ddosy.NewWeightedTargeter(pattern)
+
+	numOfIters := 1000
+	tracker := make(map[string]int)
+	for i := 0; i < numOfIters; i++ {
+		target := &vegeta.Target{}
+		targeter(target)
+		tracker[string(target.Body)] += 1
+	}
+
+	if len(tracker) != 2 {
+		t.Errorf("something dose not work with targeter, tracker size %+v", tracker)
+	}
+
+	for _, v := range tracker {
+		if v < 100 {
+			t.Errorf("Expected 50%% distribution got %d%%", v*100/numOfIters)
 		}
 	}
 }
